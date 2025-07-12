ddd3bde2760213aa36e4acec997350a456b33643
kkhushie <khushipal1470@gmail.com>
2025-07-12 10:30:12 +0530

    Initial project setup

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express=require('express')
 const app=express();
 const cors = require('cors')
-
+const cookieParser=require('cookie-parser')
 
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors())
 
 //db connection
-const connectDB = require('./config/db')
+const connectDB = require('./src/config/db')
 try {
     connectDB()
 }
